c4cc98b531244f9c5d51f34681228231a852aad1
Lautaro Llusa <llusa.lautaro@gmail.com>
2023-07-24 04:10:03 +0200

    finished home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,50 @@
+// next image
+import Image from "next/image";
+
+// components
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+
+// framer motion
+import { motion } from 'framer-motion';
+
+// variants
+import { fadeIn } from '../variants';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='bg-primary/60 h-full'>
+      {/*text*/}
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+          {/* title */}
+          <motion.h1 variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden' className='h1'>
+            Transforming Ideas <br /> Info{' '}
+            <span className='text-accent'>Digital Reality</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p variants={fadeIn('down', 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+            lorem impsum dolor sit amet consectetur adipisicing elit. itaque 
+            nostrum quam reprehenerit vro, tenetur coluptatem nulla autoaspernatur dolores ut.
+          </motion.p>
+          {/* btn */}
+          <div className='flex justify-center xl:hidden relative'>
+            <ProjectsBtn />
+          </div>
+          <motion.div variants={fadeIn('down', 0.4)} initial='hidden' animate='show' exit='hidden' className='hidden xl:flex'>
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div> 
+      {/* image */}
+    <div>
+      {/* bg img */}
+      <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0
+      '></div>
+      {/* Particles */}
+    </div>
+    </div>
+  )
 };
 
 export default Home;
