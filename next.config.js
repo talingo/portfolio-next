0ce699d8584712dd85bbf16568472fdff3c63019
Lautaro Llusa <llusa.lautaro@gmail.com>
2023-07-25 19:35:17 +0200

    ok imma try to deploy on vercel

--- next.config.js
+++ next.config.js
@@ -2,28 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-const repo = 'portfolio-next';
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '/';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
 }
 
-module.exports = {
-  nextConfig,
-  images: {
-    loader: 'imgix',
-    path: 'http://factoriaf5-4966.imgix.net/',
-  },
-
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-};
+module.exports = nextConfig
