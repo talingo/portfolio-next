4d2a23dad3387db182ce55f0690caa721cef2998
Lautaro Llusa <llusa.lautaro@gmail.com>
2023-07-25 19:25:49 +0200

    trying to deploy imgs T_T

--- next.config.js
+++ next.config.js
@@ -2,13 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+};
+
+const repo = 'portfolio-next';
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
+module.exports = {
+  nextConfig,
+  images: {
+    loader: 'imgix',
+    path: 'http://factoriaf5-4966.imgix.net/',
+  },
 
-
-
-module.exports = {nextConfig,
-images:{
-  loader: 'imgix',
-  path: 'http://factoriaf5-4966.imgix.net/'
-}}
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+};
